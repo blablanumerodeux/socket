d53a9983faa348fb7cac9f559afa81b253f501b0
blablanumerodeux <blablanumerodeux@gmail.com>
2014-10-20 11:53:47 +0200

    add the server part into the othello.c

--- othello_GUI.c
+++ othello_GUI.c
@@ -292,6 +292,77 @@ static void clique_connect_serveur(GtkWidget *b)
 {
   /***** TO DO *****/
   //se connecter au serveur du joueur adverse
+
+  int sockfd, new_fd, rv, sin_size;
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr their_adr;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; 	// use my IP
+  printf("\nNumero de port : %s\n", 5555);
+  fflush(stdout);
+  rv = getaddrinfo(NULL, 5555, &hints, &servinfo);
+  
+  if (rv != 0) 
+  {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+  
+  // Création  socket  et  attachement
+  for(p = servinfo; p != NULL; p = p->ai_next) 
+  {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+    {
+      perror("server: socket");
+      continue;
+    }
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+    {
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
+    break;
+    
+  }
+  
+  if (p == NULL)
+  {
+    fprintf(stderr, "server: failed to bind\n");
+    return 2;
+  }
+
+  freeaddrinfo(servinfo); 	// Libère structure
+
+  listen(sockfd, 5);
+  signal(SIGCHLD, SIG_IGN);
+  
+  while(1)
+  {
+    sin_size = sizeof(their_adr);
+    new_fd = accept(sockfd, &their_adr, &sin_size);
+    
+    if(!fork())
+    {
+      close(sockfd);
+      send(new_fd, "Hello!", 6, 0);
+
+    if((numbytes = recv(sockfd, buf, 100-1, 0)) == -1)
+    {
+      perror("recv");
+      exit(1);
+    }
+
+
+    close(new_fd);
+
+    //exit(0);
+    } 
+  }
+  
   //ecouter
   //si ok, alors mettre a jour l'interface et attendre le clique demarrer partie  
 }
